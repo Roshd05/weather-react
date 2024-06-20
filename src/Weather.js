0d2a07570a6a223293d873c3e2f6e9fa7b2d5af4
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-06-20 17:26:09 -0400

    change the color of the loader

--- src/Weather.js
+++ src/Weather.js
@@ -18,8 +18,8 @@ export default function Weather(props) {
         size={67}
         thickness={149}
         speed={100}
-        color="rgba(57, 74, 172, 1)"
-        secondaryColor="rgba(172, 57, 171, 0.44)"
+        color="rgba(57, 98, 172, 0.75)"
+        secondaryColor="rgba(77, 172, 57, 0.44)"
       />
     );
 }
