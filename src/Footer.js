bb9ebede4c95844083b36c522073a2b125223a38
Roshd05 <166666105+Roshd05@users.noreply.github.com>
2024-06-21 18:05:16 -0400

    fixing footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,18 +5,26 @@ export default function Footer() {
   return (
     <div>
       This project was coded by <br />
-      <a href="https://github.com/Roshd05" target="_blank">
+      <a href="https://github.com/Roshd05" target="_blank" rel="noreferrer">
         Roshni Daryanani
       </a>
       <br />
       is
       <br />
-      <a href="https://github.com/Roshd05/weather-homework" target="_blank">
+      <a
+        href="https://github.com/Roshd05/weather-homework"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-sourced on GitHub
       </a>
       <br />
       and <br />
-      <a href="https://clima-roshni.netlify.app" target="_blank">
+      <a
+        href="https://clima-roshni.netlify.app"
+        target="_blank"
+        rel="noreferrer"
+      >
         Hosted on Netlify
       </a>
     </div>
